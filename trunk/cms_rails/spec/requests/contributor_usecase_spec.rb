6cbc612b975eeda545909c1fcee759e2e3c218c0
matthewrobertson03@gmail.com <matthewrobertson03@gmail.com@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-02-03 02:29:21 +0000

    added some contributor specs

--- trunk/cms_rails/spec/requests/contributor_usecase_spec.rb
+++ trunk/cms_rails/spec/requests/contributor_usecase_spec.rb
@@ -2,4 +2,50 @@ require 'spec_helper'
 
 describe "contributor use cases" do
 
+  context "viewing projects" do
+  
+    it "should list all the projects are thiers"
+    
+    it "should list the project names"
+    
+    it "should have links to the projects"
+  
+  end
+  
+  context "searching projects" do
+  
+    it "should let them search by project name"
+    
+    it "should notify them when there are no search results"
+    
+    it "should have links to projects in results"
+  
+  end
+  
+  context "downloading resource" do
+  
+    it "should have alink to download from the project page"
+  
+  
+  end
+  
+  
+  context "adding a resource" do
+  
+  end
+  
+  
+  context "removing a resource" do
+  
+  end
+  
+  context "replacing a resource" do
+  
+  
+  end
+  
+  
+  context "creating a project" do
+  
+  end
 end
