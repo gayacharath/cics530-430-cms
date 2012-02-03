1565146dd6b1515af0b03e3d823f33dac6c07803
lillianxu415@gmail.com <lillianxu415@gmail.com@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-02-03 03:06:44 +0000

    minor additions to manager

--- trunk/cms_rails/spec/requests/manager_usecase_spec.rb
+++ trunk/cms_rails/spec/requests/manager_usecase_spec.rb
@@ -6,29 +6,42 @@ describe "manager use cases" do
     
     it 'can invite contributors to project'
     
-    it 'can kick contributors off project'
+    it 'can kick-off contributors from project'
     
-    it 'can allow contributors who request access'
+    it 'can accept membership requests from contributors'
+ 
+    it 'can send notification to individual contributor'
+    
+    it 'can view contributor profiles'
+    
   end
   
   
   context "managing resources" do
-    it 'can add resources to project'
+  
+    it 'can add new resources to project'
     
-    it 'can delete any resources to project'
+    it 'can delete any resources from project'
     
-    it 'replace resources to project'
+    it 'replace resources in project'
+    
+    it 'edit lifetime for resource'
     
     
   end
   
   
   context "managing project" do
+  
    it 'set project lietime'
   
-   it 'can delete a project that it owns'
+   it 'can delete a project that manager owns'
   
    it 'can not delete a project that it does not own'
+   
+   it 'can view notifications from contributors'
+   
+   it 'can send notifications to all contributors'
   
   end
   
