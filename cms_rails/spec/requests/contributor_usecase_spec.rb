20f79d71a55ff19eb5608e6299477d847e30287a
matthewrobertson03@gmail.com <matthewrobertson03@gmail.com@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-02-03 02:34:58 +0000

    added more specs for contributors

--- cms_rails/spec/requests/contributor_usecase_spec.rb
+++ cms_rails/spec/requests/contributor_usecase_spec.rb
@@ -25,27 +25,50 @@ describe "contributor use cases" do
   context "downloading resource" do
   
     it "should have alink to download from the project page"
-  
-  
+    
+    it "should display an error message if file not found"
+    
   end
   
   
   context "adding a resource" do
+  
+    it "there should be a link to download the resource after upload"
+  
+    it "should belong the the user that uploaded the resource"
+    
+    it "should be possible to upload from a project page"
   
   end
   
   
   context "removing a resource" do
   
+    it "should be able remove owned resource"
+    
+    it "should not be able to remove resources that aren't own"
+  
+    
+  
   end
   
   context "replacing a resource" do
-  
+    it "should be able replace owned resource"
+    
+    it "should not be able to replace resources that aren't own"
   
   end
   
   
   context "creating a project" do
+  
+    it "should make then manager for the project"
+    
+    it "there is a link from thier dashboard"
+    
+    it "should create a page for the project"
+    
+    it "should allow them to invite contributors"
   
   end
 end
