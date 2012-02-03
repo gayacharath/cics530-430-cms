9696da5c51872f0aa0e74cb52e9c497a3cb9b279
matthewrobertson03@gmail.com <matthewrobertson03@gmail.com@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-02-03 02:48:58 +0000

    added some admin specs

--- cms_rails/spec/requests/administrator_usecase_spec.rb
+++ cms_rails/spec/requests/administrator_usecase_spec.rb
@@ -2,5 +2,39 @@ require 'spec_helper'
 
 describe "admin use cases" do
 
+  context 'managing projects' do
+    
+    it 'can creat projects'
+    
+    it 'can delete any project'
+    
+    it 'can manage contributors for a project'
+    
+  end
+  
+  
+  context 'managing announcements' do
+    
+    it 'can create an announcement'
+    
+    it 'can delete any announcement'
+    
+    it 'can hide / show announcements on the front page'
+  
+  end
+  
+  
+  context 'managing users' do
+  
+    it 'can send messages to users'
+    
+    it 'can block users'
+    
+    it 'can make other users into admin users'
+    
+    it 'can edit users attributes'
+    
+    it 'can manage user priveleges'
+  end
 
 end
