536dd5dd286827262ac69127261380dd73bad7f2
lillianxu415@gmail.com <lillianxu415@gmail.com@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-02-03 02:57:41 +0000

    Added a couple new shoulds

--- cms_rails/spec/requests/contributor_usecase_spec.rb
+++ cms_rails/spec/requests/contributor_usecase_spec.rb
@@ -4,17 +4,21 @@ describe "contributor use cases" do
 
   context "viewing projects" do
   
-    it "should list all the projects are thiers"
+    it "should list all the projects they are contributing to"
     
     it "should list the project names"
     
-    it "should have links to the projects"
+    it "should have links to the project pages"
+    
+    it "should list the members of each project"
   
   end
   
   context "searching projects" do
   
     it "should let them search by project name"
+    
+    it "should let them search by member name"
     
     it "should notify them when there are no search results"
     
@@ -24,7 +28,7 @@ describe "contributor use cases" do
   
   context "downloading resource" do
   
-    it "should have alink to download from the project page"
+    it "should have a link to download from the project page"
     
     it "should display an error message if file not found"
     
@@ -32,27 +36,32 @@ describe "contributor use cases" do
   
   
   context "adding a resource" do
-  
-    it "there should be a link to download the resource after upload"
-  
-    it "should belong the the user that uploaded the resource"
     
-    it "should be possible to upload from a project page"
+    it "should show upload link on project page"
+    
+    it "should show download link after finishing upload"
+    
+    it "should show remove link after finishing upload"
+    
+    it "should show replace link after finishing upload"
   
+    it "should show as belonging to the user that uploaded the resource"
+
   end
   
   
   context "removing a resource" do
   
-    it "should be able remove owned resource"
+    it "should be able to remove owned resource"
     
-    it "should not be able to remove resources that aren't own"
+    it "should not be able to remove resources that aren't owned"
   
-    
+    it "should remove any other functionality from resource"
   
   end
   
   context "replacing a resource" do
+  
     it "should be able replace owned resource"
     
     it "should not be able to replace resources that aren't own"
@@ -61,14 +70,14 @@ describe "contributor use cases" do
   
   
   context "creating a project" do
+ 
+    it "should provide creation link on their dashboard"
   
-    it "should make then manager for the project"
+    it "should make this contributor a manager for the project"
     
-    it "there is a link from thier dashboard"
+    it "should create a new page for the project"
     
-    it "should create a page for the project"
-    
-    it "should allow them to invite contributors"
+    it "should allow this manager to invite contributors"
   
   end
 end
