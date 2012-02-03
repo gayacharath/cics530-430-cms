e721a2bee3c5f90215df4ee7cadfa0fc7bbbe424
matthewrobertson03@gmail.com <matthewrobertson03@gmail.com@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-02-03 02:42:47 +0000

    added some manager specs

--- cms_rails/spec/requests/manager_usecase_spec.rb
+++ cms_rails/spec/requests/manager_usecase_spec.rb
@@ -2,4 +2,35 @@ require 'spec_helper'
 
 describe "manager use cases" do
 
+  context "managing contributors" do
+    
+    it 'can invite contributors to project'
+    
+    it 'can kick contributors off project'
+    
+    it 'can allow contributors who request access'
+  end
+  
+  
+  context "managing resources" do
+    it 'can add resources to project'
+    
+    it 'can delete any resources to project'
+    
+    it 'replace resources to project'
+    
+    
+  end
+  
+  
+  context "managing project" do
+   it 'set project lietime'
+  
+   it 'can delete a project that it owns'
+  
+   it 'can not delete a project that it does not own'
+  
+  end
+  
+  
 end
