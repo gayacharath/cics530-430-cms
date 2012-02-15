893827c4b793c296290ec30c676df496dfd3a9ed
matthewrobertson03@gmail.com <matthewrobertson03@gmail.com@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-02-15 01:04:59 +0000

    created all the database migrations

--- cms_rails/db/schema.rb
+++ cms_rails/db/schema.rb
@@ -11,6 +11,52 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120215003832) do
+
+  create_table "announcements", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "topic"
+    t.text     "content"
+    t.string   "type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects", :force => true do |t|
+    t.datetime "started_at"
+    t.datetime "ending_at"
+    t.string   "name"
+    t.text     "destription"
+    t.integer  "owner_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "resources", :force => true do |t|
+    t.integer  "user_id"
+    t.integer  "project_id"
+    t.string   "name"
+    t.integer  "version"
+    t.string   "description"
+    t.string   "location"
+    t.string   "type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "full_name"
+    t.string   "pref_name"
+    t.string   "email"
+    t.boolean  "admin"
+    t.string   "pass_hash"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users_projects", :force => true do |t|
+    t.integer "user_id"
+    t.integer "project_id"
+  end
 
 end
