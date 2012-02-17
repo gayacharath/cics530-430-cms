42acc2ea18d1354707a6b914ff0f1938ab558378
matthewrobertson03@gmail.com <matthewrobertson03@gmail.com@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-02-17 02:31:24 +0000

    wrote some user specs

--- trunk/cms_rails/spec/models/user_spec.rb
+++ trunk/cms_rails/spec/models/user_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	context "validations" do
+
+		before(:each) do
+			@user = User.new 
+			@user.full_name = "Matthew Robertson"
+			@user.pref_name = "Matt"
+			@user.email = "matt@email.com"
+		end
+
+		it 'it starts out valid' do
+			@user.should be_valid
+		end
+
+		it 'must have a email' do
+			@user.email = nil
+			@user.should_not be_valid
+		end
+
+		it 'must have a full name'
+
+		it 'must have well formatted name'
+
+		it 'must have name that is at least 3 characters'
+
+		it 'must have a well formatted email'
+
+
+
+	end
+  
 end
