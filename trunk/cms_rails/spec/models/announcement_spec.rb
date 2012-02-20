73d5336ea7ecba7854d8c0114197060fc29988ba
bbertka <bbertka@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-02-20 20:03:31 +0000

    Added announcement spec, and stub for announcement validation

--- trunk/cms_rails/spec/models/announcement_spec.rb
+++ trunk/cms_rails/spec/models/announcement_spec.rb
@@ -1,5 +1,48 @@
 require 'spec_helper'
 
 describe Announcement do
-  
+	context "validations" do
+
+		before(:each) do
+			@announcements = Announcement.new
+			@announcements.topic= "Server Status"
+			@announcements.content= "Schedule of inactivity for February"
+      @announcements.type= "Warning"
+    end
+
+    it "starts out valid" do
+      @announcements.should be_valid
+    end
+
+		it 'must have a topic' do
+			@announcements.topic = nil
+			@announcements.should_not be_valid
+    end
+
+    it 'must have well formatted topic' do
+			@announcements.topic = '123456$%#'
+			@announcements.should_not be_valid
+    end
+
+    it 'must have a topic with minimum four characters' do
+			@announcements.topic = 'job'
+			@announcements.should_not be_valid
+		end
+
+    it 'must have content' do
+			@announcements.content = nil
+			@announcements.should_not be_valid
+    end
+
+    it 'must have content with minimum 32 characters' do
+			@announcements.content = 'some content'
+			@announcements.should_not be_valid
+    end
+
+		it 'must have a type' do
+			@announcements.type = nil
+			@announcements.should_not be_valid
+    end
+
+  end
 end
