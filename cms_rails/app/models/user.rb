cb8c4062b9cd41537c78d20f091c6ffae30c0047
bbertka <bbertka@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-02-20 19:17:58 +0000

    User validation, update. Handle all recent validations except for Unique email, and validates self, but code in there for it

--- cms_rails/app/models/user.rb
+++ cms_rails/app/models/user.rb
@@ -2,13 +2,18 @@ class User < ActiveRecord::Base
 	has_many :resources
 	has_many :announcements
 	has_many :owned_projects, :foreign_key => :owner_id, :class_name => "Project"
-
 	has_and_belongs_to_many :projects
-
   validates_associated :resources, :announcements, :owned_projects, :projects
 
+  attr_accessible :email, :full_name, :pref_name
+
 	validates_presence_of :email, :full_name, :pref_name
+  validates_inclusion_of :admin, :in => [true, false]
+
   validates_uniqueness_of :email, :case_sensitive => false
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates_format_of :email, :with => email_regex
+
   validates_length_of :full_name, :minimum => 3, :allow_nil => false, :allow_blank => false
   validates_format_of :full_name, :with => /^[a-zA-Z]{3,}$/i
 
