1fdceabdf609b3149f9788d473b186666054ff91
bbertka <bbertka@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-02-20 10:24:08 +0000

    User validations update, TODO: starts out valid, unique email, Srikanth's stuff

--- cms_rails/app/models/user.rb
+++ cms_rails/app/models/user.rb
@@ -5,26 +5,11 @@ class User < ActiveRecord::Base
 
 	has_and_belongs_to_many :projects
 
-	validates_presence_of :email
-  validates_format_of :email
-  validates_length_of :email, :allow_blank => false, :allow_nil =>false, :maximum => 256
-  validates_uniqueness_of :email
+  validates_associated :resources, :announcements, :owned_projects, :projects
 
-  validates_presence_of :full_name
-  validates_format_of :full_name
-  validates_length_of :full_name, :allow_blank => false, :allow_nil => false, :minimum => 3, :maximum => 256
-
-  validates_presence_of :pref_name
-  validates_format_of :pref_name
-  validates_length_of :pref_name, :allow_blank => true, :allow_nil => true, :maximum => 256
-
-
-# User validations should not allow admin flag to be changed by mass assignment
-# User validations must have password that is at least 6 characters
-# User validations must confirm its password exactly
-# User authentication system should return a user if password is correct
-# User authentication system should return nil if password is incorrect
-
-
+	validates_presence_of :email, :full_name, :pref_name
+  validates_uniqueness_of :email, :case_sensitive => false
+  validates_length_of :full_name, :minimum => 3, :allow_nil => false, :allow_blank => false
+  validates_format_of :full_name, :with => /^[a-zA-Z]{3,}$/i
 
 end
