aa0dad0d50d6ed4975bddf6376149e3c7b505613
srikanthramu79 <srikanthramu79@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-02-21 01:38:55 +0000

    added validation for Announcement table

--- trunk/cms_rails/app/models/announcement.rb
+++ trunk/cms_rails/app/models/announcement.rb
@@ -2,5 +2,10 @@ class Announcement < ActiveRecord::Base
 	belongs_to :user
 
   validates_presence_of :topic, :content, :type
-
+  
+  validates_length_of :topic, :minimum => 3, :maximum => 50, :allow_nil => false, :allow_blank => false
+  validates_length_of :content, :minimum => 5, :maximum => 100, :allow_nil => false, :allow_blank => false
+  validates_length_of :type, :allow_nil => false, :allow_blank => false
+  validates_format_of :topic, :with => /^[a-zA-Z]{5,}$/i # need to analyze the validation
+  # validate the allowed types like Warning, Info etc 
 end
