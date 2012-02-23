d9a1585e91f319f3c9f76301a8683719ce5aee29
matthewrobertson03@gmail.com <matthewrobertson03@gmail.com@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-02-23 21:54:12 +0000

    fixed / commented out syntax errors in specs so that they work

--- cms_rails/app/models/announcement.rb
+++ cms_rails/app/models/announcement.rb
@@ -5,7 +5,7 @@ class Announcement < ActiveRecord::Base
   
   validates_length_of :topic, :minimum => 3, :maximum => 50, :allow_nil => false, :allow_blank => false
   validates_length_of :content, :minimum => 5, :maximum => 100, :allow_nil => false, :allow_blank => false
-  validates_length_of :type, :allow_nil => false, :allow_blank => false
+  # validates_length_of :type, :allow_nil => false, :allow_blank => false
   validates_format_of :topic, :with => /^[a-zA-Z]{5,}$/i # need to analyze the validation
   # validate the allowed types like Warning, Info etc 
 end
