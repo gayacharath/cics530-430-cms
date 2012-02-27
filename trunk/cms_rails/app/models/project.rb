acacc03bf0d42adbdbc1d5f75f6c796cffa7ffa4
charathsandran@gmail.com <charathsandran@gmail.com@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-02-27 20:05:51 +0000

    validation added

--- trunk/cms_rails/app/models/project.rb
+++ trunk/cms_rails/app/models/project.rb
@@ -1,6 +1,14 @@
 class Project < ActiveRecord::Base
-
 	belongs_to :owner, :foreign_key => :owner_id, :class_name => "User"
-
 	has_and_belongs_to_many :users
+	validates_associated :users, :owner
+
+	attr_accessible :started_at, :ending_at, :name, :description, :owner_id
+
+	validates_presence_of :started_at, :ending_at, :name, :description, :owner_id
+
+	validates_length_of :name, :minimum => 3, :allow_nil => false, :allow_blank => false
+  	validates_format_of :name, :with => /^[a-zA-Z]{3,}$/i
+	validates_uniqueness_of :name, :case_sensitive => false
+
 end
