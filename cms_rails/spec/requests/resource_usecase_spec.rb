e789759f685857767ed19b841235d6840f359f15
parucleo@gmail.com <parucleo@gmail.com@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-02-28 01:38:27 +0000

    test cases for resource module

---
+++ cms_rails/spec/requests/resource_usecase_spec.rb
@@ -0,0 +1,17 @@
+﻿require 'spec_helper'
+
+describe 'the resource handler' do
+  
+  it 'should allow a contributor and project owner to create a new resource'
+  
+  it 'should allow a contributor to modify their resources only'
+  
+  it 'should allow a project owner to modify any resource in the project'
+  
+  context 'visitor is not a member' do
+  	it 'should any visitor to download a resource'
+  end
+  
+  it 'should allow contributor and project owner to download any resource in the project'
+  
+end
