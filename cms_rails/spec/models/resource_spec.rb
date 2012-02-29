7f0f49c906fd3bb62880e45ed9a94b743f620cd3
bbertka <bbertka@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-02-29 04:15:27 +0000

    Started the resource spec file.  Note, this should be done by the person assigned, and the validation should be done after the spec file is mostly completed. The validation file contains invalid variables, and should be remade.

--- cms_rails/spec/models/resource_spec.rb
+++ cms_rails/spec/models/resource_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Resource do
-  
+    before(:each) do
+      @resource = Resource.new
+      @resource.user_id=1
+      @resource.project_id=1
+      @resource.name="file.txt"
+      @resource.version=1
+      @resource.description="Test file for project"
+      @resource.location="/path_to_location"
+      @resource.type="text"
+      @resource.created_at="2012-02-15"
+      @resource.updated_at="2012-02-16"
+    end
+
+    it 'starts out valid on creation' do
+      @resource.should be_valid
+    end
+
 end
