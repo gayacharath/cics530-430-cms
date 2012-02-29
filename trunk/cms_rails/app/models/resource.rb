a52e24b7d6ae7178dba7b2e2352606dea75b81a9
bbertka <bbertka@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-02-29 04:15:27 +0000

    Started the resource spec file.  Note, this should be done by the person assigned, and the validation should be done after the spec file is mostly completed. The validation file contains invalid variables, and should be remade.

--- trunk/cms_rails/app/models/resource.rb
+++ trunk/cms_rails/app/models/resource.rb
@@ -5,16 +5,16 @@ class Resource < ActiveRecord::Base
 	validates_associated :users, :owner, :project
 	
 	attr_accessible :resourcetype, :version, :modifydate, :resourcename, :resourcecontent, :owner_id, :pid
-	
+
 	validates_presence_of :resourcetype, :version, :modifydate, :resourcename, :resourcecontent, :owner_id, :pid
-	
+
 	validates_length_of :resourctype, :minimum => 1, :maximum => 5, :allow_nil => false, :allow_blank => false
   validates_format_of :resourcetype, :with => /^[a-zA-Z]{1,}$/i
 	validates_uniqueness_of :resourcetype, :case_sensitive => false
-	
+
 	validates_length_of :resourcename, :minimum => 1, :maximum => 100, :allow_nil => false, :allow_blank => true
   validates_format_of :resourcename, :with => /^[a-zA-Z]{1,}$/i
 	validates_uniqueness_of :resourcename, :case_sensitive => false
-	
+
 	validates_uniqueness_of :resourcecontent, :case_sensitive => true
 end
