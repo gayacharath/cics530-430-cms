7c4d848ae2770a51cf37f80a954fc06e28c06582
srikanthramu79 <srikanthramu79@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-03-02 02:50:19 +0000

    Authentication related changes

--- cms_rails/app/controllers/sessions_controller.rb
+++ cms_rails/app/controllers/sessions_controller.rb
@@ -1,7 +1,25 @@
 class SessionsController < ApplicationController
 
-	def new
-		render :new
-	end
+	#def new
+		#render :new
+	#end
+	
+#Authentication Code Begin
+def create
+  user = User.find_by_email(params[:email])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect_to user_url(:id => user.id), :notice => "Logged in!"
+  else
+    flash.now.alert = "Invalid email or password"
+    render "new"
+  end
+end
+
+def destroy
+  session[:user_id] = nil
+  redirect_to root_url, :notice => "Logged out!"
+end
+#Authentication Code End
 
 end
