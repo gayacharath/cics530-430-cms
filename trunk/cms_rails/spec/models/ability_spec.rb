837058721a25eec049850955d6daf81c5f47fefa
bbertka <bbertka@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-03-02 02:56:53 +0000

    added basic tests in ability spec

--- trunk/cms_rails/spec/models/ability_spec.rb
+++ trunk/cms_rails/spec/models/ability_spec.rb
@@ -9,11 +9,18 @@ describe Ability do
 
 
     context "not_admin" do
-
       before(:each) do
         @user = Factory(:user)
+        @ability = Ability.new(@user)
       end
 
+      it "can edit themselves" do
+        @ability.should be_able_to(:edit, @user)
+      end
+
+      it "can not edit another user" do
+        @ability.should_not be_able_to(:edit, @user)
+      end
     end
 
 end
