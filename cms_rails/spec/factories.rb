58c97ed401787e5911708cf8e52894041947ad1c
bbertka <bbertka@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-03-02 02:45:22 +0000

    Added FactoryGirl object definitions

--- cms_rails/spec/factories.rb
+++ cms_rails/spec/factories.rb
@@ -1,1 +1,37 @@
 require 'factory_girl'
+
+FactoryGirl.define do
+
+  factory :user do
+    full_name 'Matthew Robertson'
+    pref_name 'Matt'
+    email 'matt@email.com'
+  end
+
+  factory :announcement do
+    topic= "Server Status"
+		content= "Schedule of inactivity for February"
+    type= "Warning"
+  end
+
+  factory :project do
+    started_at = "2012-02-15"
+    ending_at = "2012-03-15"
+    name = "CICS-530"
+    description = "Term project for MSS students based on content management system"
+    owner_id = 1
+  end
+
+  factory :resource do
+    user_id=1
+    project_id=1
+    name="file.txt"
+    version=1
+    description="Test file for project"
+    location="/path_to_location"
+    type="text"
+    created_at="2012-02-15 12:30:24"
+    updated_at="2012-02-16 12:31:50"
+  end
+
+end
--- cms_rails/spec/models/resource_spec.rb
+++ cms_rails/spec/models/resource_spec.rb
@@ -2,16 +2,7 @@ require 'spec_helper'
 
 describe Resource do
     before(:each) do
-      @resource = Resource.new
-      @resource.user_id=1
-      @resource.project_id=1
-      @resource.name="file.txt"
-      @resource.version=1
-      @resource.description="Test file for project"
-      @resource.location="/path_to_location"
-      @resource.type="text"
-      @resource.created_at="2012-02-15 12:30:24"
-      @resource.updated_at="2012-02-16 12:31:50"
+      @resource = Factory(:resource)
     end
 
     it 'starts out valid on creation' do
