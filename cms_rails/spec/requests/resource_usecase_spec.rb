e073652b9a86bcf8a67017d2cf6f1063f6309836
parucleo@gmail.com <parucleo@gmail.com@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-03-02 10:31:00 +0000

    updated resource fixture, resource spec for models and resource spec for requests: minor variable changes

--- cms_rails/spec/requests/resource_usecase_spec.rb
+++ cms_rails/spec/requests/resource_usecase_spec.rb
@@ -9,7 +9,11 @@ describe 'the resource handler' do
   it 'should allow a project owner to modify any resource in the project'
   
   context 'visitor is not a member' do
-  	it 'should any visitor to download a resource'
+  	it 'should allow any visitor to download a resource'
+
+    it 'should not allow visitor to modify a resource'
+
+    it 'should not allow a visitor to create a new resource'
   end
   
   it 'should allow contributor and project owner to download any resource in the project'
