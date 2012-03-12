056527078149de19c52ba40b3b7437663c8f02ba
matthewrobertson03@gmail.com <matthewrobertson03@gmail.com@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-03-12 07:29:33 +0000

    added some more specs

--- cms_rails/app/controllers/sessions_controller.rb
+++ cms_rails/app/controllers/sessions_controller.rb
@@ -1,29 +1,27 @@
 class SessionsController < ApplicationController
 
-def new
-  if current_user
-    redirect_to user_path(current_user), :alert => "You are already logged in"
-  else
-    render
+  def new
+    if current_user
+      redirect_to user_path(current_user), :alert => "You are already logged in"
+    else
+      render
+    end
   end
-end
-	
-#Authentication Code Begin
-def create
-  user = User.find_by_email(params[:email])
-  if user && user.authenticate(params[:password])
-    session[:user_id] = user.id
-    redirect_to user_path(current_user), :notice => "Logged in!"
-  else
-    flash.now.alert = "Invalid email or password"
-    render "new"
+  	
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_path(current_user), :notice => "Logged in!"
+    else
+      flash.now.alert = "Invalid email or password"
+      render "new"
+    end
   end
-end
 
-def destroy
-  session[:user_id] = nil
-  redirect_to root_url, :notice => "Logged out!"
-end
-#Authentication Code End
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, :notice => "Logged out!"
+  end
 
 end
