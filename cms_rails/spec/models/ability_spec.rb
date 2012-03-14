a6c93aab654b3ed2f8490c1700d27704b5dc54ad
bbertka <bbertka@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-03-14 00:35:05 +0000

    Working on CanCan abilities for non_admin

--- cms_rails/spec/models/ability_spec.rb
+++ cms_rails/spec/models/ability_spec.rb
@@ -21,6 +21,48 @@ describe Ability do
       it "can not edit another user" do
         @ability.should_not be_able_to(:edit, @user)
       end
+
+      it "can view their associated projects" do
+
+      end
+
+      it "can not view other user's associated projects" do
+
+      end
+
+      it "can delete owned resource" do
+
+      end
+
+      it "can not delete other user's resource"  do
+
+      end
+
+      it "can replace owned resource" do
+
+      end
+
+      it "can not replace other user's owned resource" do
+
+      end
+
+      it "can modify owned resource" do
+
+      end
+
+      it "can not modify other user's owned resource" do
+
+      end
+
+      it "can download associated project resource" do
+
+      end
+
+      it "can not download non-associated project resource"  do
+
+      end
+
+
     end
 
 end
