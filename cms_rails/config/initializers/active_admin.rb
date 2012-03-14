a70b59d8958e8071cda68a91293c28756ee0b512
lillianxu415@gmail.com <lillianxu415@gmail.com@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-03-14 00:30:59 +0000

    change the page title

--- cms_rails/config/initializers/active_admin.rb
+++ cms_rails/config/initializers/active_admin.rb
@@ -5,7 +5,7 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Cms Rails"
+  config.site_title = "Project Box"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
