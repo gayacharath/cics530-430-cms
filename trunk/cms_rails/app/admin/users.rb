cef2b19c965e235eb47436c3a4c98869d2514924
lillianxu415@gmail.com <lillianxu415@gmail.com@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-03-16 05:39:24 +0000

    add operations of admin

--- trunk/cms_rails/app/admin/users.rb
+++ trunk/cms_rails/app/admin/users.rb
@@ -1,15 +1,40 @@
 ActiveAdmin.register User do
 	scope :administrator
+	
  
   index do
 	column  "Full name", :full_name
     column  "Nick name", :pref_name
     column  "Email", :email
-    column  "Is admin?", :admin?
+    column  "Is admin?", :admin? 
+		
     column  "Create date", :created_at
     column  "Last modified", :updated_at
 	default_actions
   end
+  
+ # edit do
+  
+	#form do |f|
+	#	  f.inputs "Details" do
+	#		f.input :full_name
+	#		f.input :pref_name, :label => "Nick name"
+	#		f.input :email
+	#	  end
+	#	end
+	#end
+  
+  
+  action_item :only => :edit do
+		if(user.admin == true)
+		link_to "Remove admin", remove_admin_path(user)
+		else
+		link_to "Set as admin", create_admin_path(user)
+	end
+		  
+  end
+
+
 
   #if they are an admin
   # link_to "make admin", create_admin_path(user)
