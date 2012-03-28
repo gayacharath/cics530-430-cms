6151049b9c0fbb0df0930fa351edd141dd35a44c
matthewrobertson03@gmail.com <matthewrobertson03@gmail.com@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-03-28 02:17:29 +0000

    added backbone to the project for mobile

--- trunk/cms_rails/app/controllers/sessions_controller.rb
+++ trunk/cms_rails/app/controllers/sessions_controller.rb
@@ -14,10 +14,16 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to user_path(current_user), :notice => "Logged in!"
+      respond_to do |format|
+        format.html { redirect_to user_path(current_user), :notice => "Logged in!" }
+        format.json { render :json => user.to_json }
+      end
     else
       flash.now.alert = "Invalid email or password"
-      render "new"
+      respond_to do |format|
+        format.html { render "new" }
+        format.json { render :json => {error: "invalid user name or password"}.to_json }
+      end
     end
   end
 
