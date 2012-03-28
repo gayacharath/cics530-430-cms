27db9cc36c5f498e6104146cccc41305600763ed
lillianxu415@gmail.com <lillianxu415@gmail.com@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-03-28 23:40:35 +0000

    add new functions to project page

--- cms_rails/app/admin/projects.rb
+++ cms_rails/app/admin/projects.rb
@@ -1,23 +1,24 @@
 ActiveAdmin.register Project do
-	member_action :projectmanagement do
-      @project = Project.find(params[:id])
+	sidebar "Resources", :only =>:show do
+		table_for Resource.where(:project_id => self.project) do |t|
+		t.column("Resource") { |resource| link_to resource.name, admin_resource_path(resource) }
+  end
+		
 	end
+	
+	
+	
   
   index do
     column "Project Name", :name
     column "Start Date", :started_at
     column "End Date", :ending_at
     column "Owner", :owner
-    column "Contributors" do |proj|
-      names = ""
-       proj.users.each do |user|
-        names += user.name + " "
-       end
-       names
-     end
 	   column "Description", :description
 	   column "Created date", :created_at
 	 column "Last modified", :updated_at
     default_actions
   end
+ 
+  
 end
