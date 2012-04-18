b70ae6f6f8b9b964ec732b9b3875b73babdfbe0a
charathsandran@gmail.com <charathsandran@gmail.com@988d4ddd-2ca7-dc6f-8f90-5d4ec8aa1115>
2012-04-18 02:25:32 +0000

    updated

--- trunk/cms_rails/app/mailers/user_mailer.rb
+++ trunk/cms_rails/app/mailers/user_mailer.rb
@@ -1,3 +1,14 @@
 class UserMailer < ActionMailer::Base
   default from: "from@example.com"
+
+  def   password_reset(user)
+  	@user = user
+  	mail :to => user.email, :subject => "ProjectBox Password Reset Request"
+  end
+
+  def   invitation(user, project)
+  	@user = user
+	@project = project
+  	mail :to => user.email, :subject => "Request to join the project"
+  end
 end
